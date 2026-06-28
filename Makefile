7712b9b6f5b90f0e24f2309678010092c269a56c
Water Chika <water_chika@outlook.com>
2026-06-29 01:58:29 +0800

    update switch_vt

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
+all: test_drm switch_vt
+
 test_drm: main.c
 	gcc main.c -I/usr/include/libdrm -ldrm -o $@
+
+switch_vt: switch_vt.c
+	gcc switch_vt.c -o $@
--- switch_vt.c
+++ switch_vt.c
@@ -2,17 +2,32 @@
 #include <linux/vt.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
-#include <assert.h>
-#include <limits.h>
-#include <poll.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
+int main(int argc, char** argv) {
+    int vt = 0;
+    if (argc < 2) {
+        fprintf(stderr, "Usage: switch_vt <vt>\n");
+        return -1;
+    }
+    char* endptr;
+    vt = strtol(argv[1], &endptr, 10);
+    if (endptr == argv[1]) {
+        fprintf(stderr, "vt sould be a number\n");
+        return -1;
+    }
+
     int fd = open("/dev/tty0", O_RDWR);
-    assert(fd);
-    int ret = ioctl(fd, VT_ACTIVATE, 4);
-    assert(ret!=-1);
-    ret = ioctl(fd, VT_WAITACTIVE, 2);
-    assert(ret != -1);
+    if (-1 == fd) {
+        fprintf(stderr, "open tty0 fail\n");
+        return -1;
+    }
+    int ret = ioctl(fd, VT_ACTIVATE, vt);
+    if (-1 == ret) {
+        fprintf(stderr, "active vt failed\n");
+        return -1;
+    }
     close(fd);
     return 0;
 }
